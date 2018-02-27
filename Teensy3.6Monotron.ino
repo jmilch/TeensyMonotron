2d58d7b5e6580d8fb6d7a94879c11b1d51182a7a
jmilch <36661409+jmilch@users.noreply.github.com>
2018-02-27 16:45:26 +0100

    Update Teensy3.6Monotron.ino

--- Teensy3.6Monotron.ino
+++ Teensy3.6Monotron.ino
@@ -4,9 +4,10 @@ float frequenzen[128];
 
 const int subOscPin = 3;
 const int ledPin = 13;
-const int pitchCVPin = A21;
-const int filterCutoffCVPin = A22;
-const int gatePin = 29;
+const int pitchCVPin = A12; //DAC1 A21 on Teensy 3.6 and A12 on Teensy LC
+//Filter Pin on second DAC uncomment for Teensy 3.6
+//const int filterCutoffCVPin = A22;
+const int gatePin = 17;
 byte subOscTranspose = 0;
 float bend;
 float pitch_value;
@@ -43,8 +44,9 @@ void OnNoteOn(byte kanal, byte note, byte velocity) {
     digitalWrite(ledPin, HIGH);
     pitch_value = constrain(note, 60, 99);
     analogWrite(pitchCVPin, ((pitch_value + bend - 60.0) / divider) * 4095.0);
-    analogWrite(filterCutoffCVPin, map(velocity, 0, 127, 0, 4095));
-    digitalWriteFast(29, HIGH);
+    //Filter Pin on second DAC uncomment for Teensy 3.6
+    //analogWrite(filterCutoffCVPin, map(velocity, 0, 127, 0, 4095));
+    digitalWriteFast(gatePin, HIGH);
     tone(subOscPin, frequenzen[note+subOscTranspose]);
   }
 }
